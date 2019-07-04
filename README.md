4a30fcfb0b80129f7e5c619600ed2b12f2b3b1ad
Yidadaa <yidadaa@qq.com>
2019-07-04 22:54:36 +0800

    feat(data): 完善数据处理脚本

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Ali-Agriculture-AI-Challenge-2019
 阿里天池2019年县域农业大脑AI挑战赛
 
+## 依赖库
+```
+1. pyvips
+sudo apt install libvips
+pip install pyvips
+
+2. opencv
+pip install opencv-python
+```
+
 ## 数据预处理
 1. 将RGBA图片处理为RGB图片，其中透明度为0的区域像素值设置为`(0, 0, 0)`，其他则直接提取出RGB通道。
 2. 数据增强，包括多尺度裁剪、随机裁剪、多角度旋转（0/15/30/45/60/75/90）、镜像、翻转等。
---
+++ data/make_train_test.py
@@ -0,0 +1,158 @@
+'''
+数据处理函数
+'''
+import os
+from itertools import product
+from argparse import ArgumentParser
+
+from pyvips import Image as vipImage
+import numpy as np
+from cv2 import cv2
+from tqdm import tqdm
+
+def mk_folder(path:str):
+    '''创建目录
+
+    Args:
+        path(str): 目标路径
+    '''
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print('{} created!'.format(path))
+
+def create_folders(path:str):
+    '''生成对应的目录
+
+    Args:
+        path(str): 目标路径
+
+    Return:
+        此函数在$path路径下生成img和label两个路径
+    '''
+    mk_folder(path)
+    
+    for folder in ['img', 'label']:
+        full_path = os.path.join(path, folder)
+        mk_folder(full_path)
+
+def load_to_memory(img)->np.ndarray:
+    '''将图片完全加载到内存，低于32GB内存的机器慎用
+
+    Args:
+        img: pyvips image对象
+
+    Return:
+        img_arr: numpy数组
+    '''
+    format_to_dtype = {
+        'uchar': np.uint8,
+        'char': np.int8,
+        'ushort': np.uint16,
+        'short': np.int16,
+        'uint': np.uint32,
+        'int': np.int32,
+        'float': np.float32,
+        'double': np.float64,
+        'complex': np.complex64,
+        'dpcomplex': np.complex128,
+    }
+    return np.ndarray(
+        buffer=img.write_to_memory(),
+        dtype=format_to_dtype[img.format],
+        shape=[img.height, img.width, img.bands]
+    )
+
+def is_img_empty(img_array:np.ndarray, threshold:float)->bool:
+    '''判断图片是否为空
+
+    Args:
+        img_array(np.ndarray): 表示图像的numpy数组
+        threshold(float): 空白区域占比，大于这个比例，将返回False
+
+    Return:
+        is_empty(bool): 是否为空
+    '''
+    if not (len(img_array.shape) == 3 and img_array.shape[-1] == 4):
+        raise ValueError('img_array应该为四通道图像')
+    
+    w, h = img_array.shape[0:2]
+    opacity_mask = img_array[:, :, 3] # 抽取透明度通道
+    opacity_ratio = np.sum([opacity_mask == 0]) / w / h # 统计透明像素占比
+    return opacity_ratio < threshold
+
+def extract_and_save(img_path:str, label_path:str, output_path:str):
+    '''读取图像并进行切分处理
+
+    Args:
+        img_path(str): 要处理的图像路径
+        label_path(str): 对应的标签图像
+    '''
+    # 获取要处理的图片文件名
+    img_file_name = os.path.basename(img_path).rsplit('.')[0]
+    # 获取输出目录的路径
+    src_output_path = os.path.join(output_path, 'img')
+    label_output_path = os.path.join(output_path, 'label')
+
+    print('Loading source image from {}.'.format(img_path))
+    src_img = vipImage.new_from_file(img_path)
+    
+    print('Loading label image from {}.'.format(label_path))
+    label_img = vipImage.new_from_file(label_path)
+
+    # 输出图片基本信息
+    for img in [('Source Image', src_img), ('Label Image', label_img)]:
+        print('{} info:'.format(img[0]))
+        for key in ['width', 'height', 'bands']:
+            print('\t{}: {}'.format(key, getattr(img[1], key)))
+
+    # 将图像完全加载到内存，耗时可能会很长，内存小的机器慎用
+    print('Loading image to memory, it may take a few minutes.')
+    src_img = load_to_memory(src_img)
+    label_img = load_to_memory(label_img)
+
+    # 开始裁剪图片
+    scales = [240, 480, 960, 1280] # 多尺度裁切
+    build_iter = lambda n, s: range(0, (n // s + 1) * s, s) # 范围覆盖(0, n)且步长为s的迭代器
+
+    # 统计图像总数目
+    img_count = 0
+
+    for scale in scales:
+        # 这里会根据实际图像大小进行裁剪
+        [w, h] = src_img.shape[0:2]
+        # 使用两个迭代器完成裁切工作
+        width_iter = build_iter(w, scale)
+        height_iter = build_iter(h, scale)
+        w_h_iter = product(width_iter, height_iter) # product生成两个迭代器的笛卡尔积
+        total = len(width_iter) * len(height_iter) # 裁剪后图像总数量
+        img_count += total
+        for (x, y) in tqdm(w_h_iter, desc='Scale[{}*{}]'.format(scale, scale), total=total):
+            # 执行裁剪
+            cropped_src_img = src_img[x:x + scale, y:y + scale, :] # 只抽取RGB通道
+            cropped_label_img = label_img[x:x + scale, y:y + scale, :]
+            # 过滤掉空白区域
+            if not is_img_empty(cropped_src_img, 0.95):
+                # 写入硬盘
+                filename = '{}-{}-{}-{}.png'.format(scale, x, y, img_file_name)
+                # 只需写入RGB值
+                cv2.imwrite(os.path.join(src_output_path, filename), cropped_src_img[:, :, 0:3])
+                cv2.imwrite(os.path.join(label_output_path, filename), cropped_label_img)
+
+    print('Extracted {} images from {}'.format(img_count, img_path))
+
+def parse_args():
+    '''解析命令行参数'''
+    arg_parser = ArgumentParser(usage='python3 make_train_test.py -i path/to/large/img -l path/to/label/img -o path/to/output')
+    arg_parser.add_argument('-i', '--img', help='卫星图路径')
+    arg_parser.add_argument('-l', '--label', help='卫星图标签图路径')
+    arg_parser.add_argument('-o', '--output', help='输出路径')
+
+    return arg_parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    assert os.path.exists(args.img), '请指定有效的图片路径'
+    assert os.path.exists(args.label), '请指定有效的标签图片路径'
+    assert args.output is not None, '请指定有效果的输出路径'
+    create_folders(args.output)
+    extract_and_save(args.img, args.label, args.output)
