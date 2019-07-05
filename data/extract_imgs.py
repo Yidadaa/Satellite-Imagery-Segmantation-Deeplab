1f8eaa04574ba973111baa25fd9430e595ec9df3
Yidadaa <yidadaa@qq.com>
2019-07-05 19:20:35 +0800

    feat(data): 计算数据集的均值和方差

--- data/extract_imgs.py
+++ data/extract_imgs.py
@@ -126,7 +126,7 @@ def extract_and_save(img_path:str, label_path:str, output_path:str):
         w_h_iter = product(width_iter, height_iter) # product生成两个迭代器的笛卡尔积
         total = len(width_iter) * len(height_iter) # 裁剪后图像总数量
         img_count += total
-        imgs = None
+        imgs = []
         for (x, y) in tqdm(w_h_iter, desc='Scale[{}*{}]'.format(scale, scale), total=total):
             # 执行裁剪
             cropped_src_img = src_img[x:x + scale, y:y + scale, :] # 只抽取RGB通道
@@ -135,15 +135,16 @@ def extract_and_save(img_path:str, label_path:str, output_path:str):
             if not is_img_empty(cropped_src_img, 0.95):
                 # 收集所有的像素值，用于计算均值和方差
                 w, h, c = cropped_src_img.shape
-                reshaped_crop = cropped_src_img.reshape((w * h, c))
-                print(imgs.shape if imgs is not None else None, reshaped_crop.shape)
-                imgs = reshaped_crop if imgs is None else np.concatenate(imgs, reshaped_crop)
+                reshaped_crop = cropped_src_img.reshape((w * h, c))[:, 0:3]
+                imgs.append(reshaped_crop)
                 # 写入硬盘
                 filename = '{}-{}-{}-{}.png'.format(scale, x, y, img_file_name)
                 # 只需写入RGB值
                 cv2.imwrite(os.path.join(src_output_path, filename), cropped_src_img[:, :, 0:3])
                 cv2.imwrite(os.path.join(label_output_path, filename), cropped_label_img)
         # 计算数据集的均值和方差
+        print('concating imgs')
+        imgs = np.concatenate(imgs)
         print(imgs.shape)
         mean = list(np.mean(imgs, axis=0))
         std = list(np.std(imgs, axis=0))
